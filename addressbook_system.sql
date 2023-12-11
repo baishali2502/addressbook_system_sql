a563711f6082fbb47159ef483e106a30e6efdef7
baishali2502 <baishali2502@gmail.com>
2023-12-11 07:06:58 +0530

    Added UC2:Create AddressBook database

--- addressbook_system.sql
+++ addressbook_system.sql
@@ -3,3 +3,15 @@ CREATE DATABASE IF NOT EXISTS AddressBook;
 
 -- Switch to the AddressBook database
 USE AddressBook;
+
+CREATE TABLE IF NOT EXISTS AddressBook (
+    ContactID INT AUTO_INCREMENT PRIMARY KEY,
+    FirstName VARCHAR(50) NOT NULL,
+    LastName VARCHAR(50) NOT NULL,
+    Address VARCHAR(255),
+    City VARCHAR(50),
+    State VARCHAR(50),
+    Zip VARCHAR(10),
+    Phone VARCHAR(20),
+    Email VARCHAR(100)
+);
